023a1ed92227271bbb86439bdbe198fdce25417a
davidkreiner <david.gerhard.kreiner@gmail.com>
2025-12-03 18:27:25 +0100

    Refactor RunSCM.R for better structure and clarity

--- RunSCM.R
+++ RunSCM.R
@@ -1,22 +1,29 @@
+
 # --------------------------------------------------------------
-# 1. Define relevant Parameters:
+# 1. Load required packages
+# --------------------------------------------------------------
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+pacman::p_load(Synth, dplyr, ggplot2, stringr, knitr, kableExtra, webshot2, tidyr,purrr,parallel)
+
+
+
+# --------------------------------------------------------------
+# 2. Define relevant Parameters:
 # --------------------------------------------------------------
 
 country <- "IT" # Treated country
 CONTROL_NUTS_CODE <- c("DE", "NL", "FI", "DK", "SE", "IE", "LU")
 num_cores <- detectCores()-1
-number_permutations <- 1500
+number_permutations <- 150
 predictors <- c("LN_POP","DENS","CAPITAL","EDUC")
 Output_folder <- "Output"
 
 
-# --------------------------------------------------------------
-# 2. Load required packages and create directories
-# --------------------------------------------------------------
-if (!require("pacman")) install.packages("pacman")
-library(pacman)
-pacman::p_load(Synth, dplyr, ggplot2, stringr, knitr, kableExtra, webshot2, tidyr,purrr)
 
+# --------------------------------------------------------------
+# 3. Create directories
+# --------------------------------------------------------------
 if(!dir.exists(Output_folder)) dir.create(Output_folder)
 
 subfolders <- c("plots/results", "plots/placebo")
@@ -26,8 +33,10 @@ for(sub in subfolders){
   folder_path <- here::here(Output_folder, sub)
   if(!dir.exists(folder_path)) dir.create(folder_path, recursive = TRUE)
 }
+
+
 # --------------------------------------------------------------
-# 3. Run scripts
+# 4. Run scripts
 # --------------------------------------------------------------
 source(here::here("Scripts","Functions.R"))
 source(here::here("Scripts","Dataset.R"))
@@ -39,8 +48,5 @@ source(here::here("Scripts","Placebo.R"))
 rmarkdown::render(input = here::here("BA_Github.rmd"),output_format="pdf_document",
                   params = list(Output_folder = Output_folder),
                   envir = globalenv())
-
-
-
 
 
